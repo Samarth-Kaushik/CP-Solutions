c9cb5b136919c3aeffb5810efa618bc58ddc7794
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-11 11:51:26 +0530

    Add House-Robber (dynamic-programming)

--- leetcode/dynamic-programming/198-House-Robber.cpp
+++ leetcode/dynamic-programming/198-House-Robber.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-    int solve(int ind, vector<int>& nums, vector<int>& dp){
-        if(ind == 0) return nums[ind];
-        if(ind < 0) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int pick = nums[ind] + solve(ind - 2, nums, dp);
-        int notpick = 0 + solve(ind - 1, nums, dp);
-        return dp[ind] = max(pick, notpick);
-    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        return solve(n-1, nums, dp);
+        dp[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            int take = nums[i];
+            if(i > 1) take += dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = max(take, nottake);
+        }
+        return dp[n-1];
     }
 };
