172748e761a533e2f86335ff24bb3be98addbf33
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-12 16:10:18 +0530

    Add Unique-Paths (dynamic-programming)

--- leetcode/dynamic-programming/62-Unique-Paths.cpp
+++ leetcode/dynamic-programming/62-Unique-Paths.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int>& dp, int n){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
-        if(dp[i*n+j] != -1) return dp[i*n+j];
-        int down = solve(i-1, j, dp, n);
-        int right = solve(i, j-1, dp, n);
-        return dp[i*n+j] = (down + right);
-    }
     int uniquePaths(int m, int n) {
-        vector<int> dp(m*n, -1);
-        return solve(m-1, n-1, dp, n);
-        // return dp[0];
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) dp[0][0] = 1;
+                else{int up = 0, left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
