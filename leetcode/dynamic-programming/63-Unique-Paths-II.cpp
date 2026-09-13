d68ed87c7116dbde9d8e5baea3956ade191ff745
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-13 18:53:14 +0530

    Add Unique-Paths-II (dynamic-programming)

--- leetcode/dynamic-programming/63-Unique-Paths-II.cpp
+++ leetcode/dynamic-programming/63-Unique-Paths-II.cpp
@@ -4,19 +4,19 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         //out of bound
-        if(i > n-1 || j > m-1 || obstacleGrid[i][j] == 1) return 0;
+        if(i < 0 || j < 0 || obstacleGrid[i][j] == 1) return 0;
         //base case
-        if(i == n-1 && j == m-1) return 1; 
+        if(i == 0 && j == 0) return 1; 
         if(dp[i][j] != -1) return dp[i][j];
-        int down = solve(i, j+1, obstacleGrid, dp);
-        int right = solve(i+1, j, obstacleGrid, dp);
-        return dp[i][j] = down + right;
+        int up = solve(i, j-1, obstacleGrid, dp);
+        int left = solve(i-1, j, obstacleGrid, dp);
+        return dp[i][j] = up + left;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(0, 0, obstacleGrid, dp);
+        return solve(n-1, m-1, obstacleGrid, dp);
     }
 };
