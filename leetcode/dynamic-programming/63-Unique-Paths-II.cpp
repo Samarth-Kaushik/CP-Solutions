9c391434d9bf932d6e5d2be141a44932c25548bb
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-13 18:59:31 +0530

    Add Unique-Paths-II (dynamic-programming)

--- leetcode/dynamic-programming/63-Unique-Paths-II.cpp
+++ leetcode/dynamic-programming/63-Unique-Paths-II.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        //out of bound
-        if(i < 0 || j < 0 || obstacleGrid[i][j] == 1) return 0;
-        //base case
-        if(i == 0 && j == 0) return 1; 
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = solve(i, j-1, obstacleGrid, dp);
-        int left = solve(i-1, j, obstacleGrid, dp);
-        return dp[i][j] = up + left;
-    }
+    // int solve(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+    //     int n = obstacleGrid.size();
+    //     int m = obstacleGrid[0].size();
+    //     //out of bound
+    //     if(i < 0 || j < 0 || obstacleGrid[i][j] == 1) return 0;
+    //     //base case
+    //     if(i == 0 && j == 0) return 1; 
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int up = solve(i, j-1, obstacleGrid, dp);
+    //     int left = solve(i-1, j, obstacleGrid, dp);
+    //     return dp[i][j] = up + left;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1) return 0;
         vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solve(n-1, m-1, obstacleGrid, dp);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int left = 0, down = 0;
+                    if(j > 0) down = dp[i][j-1];
+                    if(i > 0) left = dp[i-1][j];
+                    dp[i][j] = down + left;
+                }
+            }
+        }
+        return dp[n-1][m-1];
+        // return solve(n-1, m-1, obstacleGrid, dp);
     }
 };
