ce3073286aaeff860dedb0bd318ff6fe39c3431e
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-14 14:58:25 +0530

    Add Minimum-Path-Sum (dynamic-programming)

--- leetcode/dynamic-programming/64-Minimum-Path-Sum.cpp
+++ leetcode/dynamic-programming/64-Minimum-Path-Sum.cpp
@@ -3,20 +3,21 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        vector<int> dp(n, 0);
         for(int i = 0; i < m; i++){
             for(int j = 0; j  < n; j++){
-                if(i == 0 && j == 0) continue;
+                if(i == 0 && j == 0) dp[0] = grid[0][0];
                 else if(i == 0){
-                    grid[i][j] += grid[i][j-1];
+                    dp[j] = grid[i][j] + dp[j-1];
                 }
                 else if(j == 0){
-                    grid[i][j] += grid[i-1][j];
+                    dp[j] = grid[i][j] + dp[j];
                 }
                 else{
-                    grid[i][j] += min(grid[i][j-1], grid[i-1][j]);
+                    dp[j] = grid[i][j] + min(dp[j], dp[j-1]);
                 }
             }
         }
-        return grid[m-1][n-1];
+        return dp[n-1];
     }
 };
