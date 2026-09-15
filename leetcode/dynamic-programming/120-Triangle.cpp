f678150c753c9731d87027625ea65100384d3f5e
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-15 15:28:42 +0530

    Add Triangle (dynamic-programming)

---
+++ leetcode/dynamic-programming/120-Triangle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+        int m = triangle.size();
+        int n = triangle[0].size();
+        if(i == m-1) return dp[i][j] = triangle[i][j];
+        if(dp[i][j] != INT_MIN) return dp[i][j];
+        int bottom = INT_MAX, right = INT_MAX;
+        bottom = triangle[i][j] + solve(i+1, j, triangle, dp);
+        right = triangle[i][j] + solve(i+1, j+1, triangle, dp);
+        return dp[i][j] = min(right, bottom);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MIN));
+        // for(int i = 0; i < )
+        return solve(0, 0, triangle, dp);
+    }
+};
