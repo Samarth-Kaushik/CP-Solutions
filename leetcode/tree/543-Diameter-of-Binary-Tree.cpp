4d5a15d93424be511ea6bcdccbbf4713f0054799
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-16 22:57:32 +0530

    Add Diameter-of-Binary-Tree (tree)

--- leetcode/tree/543-Diameter-of-Binary-Tree.cpp
+++ leetcode/tree/543-Diameter-of-Binary-Tree.cpp
@@ -11,17 +11,19 @@
  */
 class Solution {
 public:
-    int height(TreeNode* root){
-        if(root == nullptr) return 0;
-        int l = height(root->left);
-        int r = height(root->right);
-        return 1 + max(l, r);
+    pair<int, int> diameter(TreeNode* root){
+        if(root == NULL) return {0,0};
+        pair<int, int> right = diameter(root->right);
+        pair<int, int> left = diameter(root->left);
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = right.second + left.second;
+        pair<int, int> ans;
+        ans.first = max({op1, op2, op3});
+        ans.second = max(left.second, right.second) + 1;
+        return ans;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL) return 0;
-        int op1 = diameterOfBinaryTree(root->left);
-        int op2 = diameterOfBinaryTree(root->right);
-        int op3 = height(root->left) + height(root->right);
-        return max({op1, op2, op3});
+        return diameter(root).first;
     }
 };
