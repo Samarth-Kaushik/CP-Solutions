3d9915fec1b5a0313df6eedbacb0e10716e7b4cd
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-22 16:08:51 +0530

    Add Partition-Equal-Subset-Sum (dynamic-programming)

--- leetcode/dynamic-programming/416-Partition-Equal-Subset-Sum.cpp
+++ leetcode/dynamic-programming/416-Partition-Equal-Subset-Sum.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    // bool solve(int ind, int target, vector<int>& nums, vector<vector<int>>& dp){
-    //     if(target == 0) return true;
-    //     if(ind == 0) return nums[0] == target;
-    //     if(dp[ind][target] != -1) return dp[ind][target];
-    //     bool notTake = solve(ind-1, target, nums, dp);
+    // bool solve(int ind, int sum/2 , vector<int>& nums, vector<vector<int>>& dp){
+    //     if(sum/2  == 0) return true;
+    //     if(ind == 0) return nums[0] == sum/2 ;
+    //     if(dp[ind][sum/2 ] != -1) return dp[ind][sum/2 ];
+    //     bool notTake = solve(ind-1, sum/2 , nums, dp);
     //     bool take = false;
-    //     if(nums[ind] <= target){
-    //         take = solve(ind-1, target-nums[ind], nums, dp);
+    //     if(nums[ind] <= sum/2 ){
+    //         take = solve(ind-1, sum/2 -nums[ind], nums, dp);
     //     }
-    //     return dp[ind][target] = take || notTake;
+    //     return dp[ind][sum/2 ] = take || notTake;
     // }
     bool canPartition(vector<int>& nums) {
         int sum = 0;
         for(int num : nums) sum += num;
         int n = nums.size();
         if(sum&1) return false;
-        vector<vector<int>> dp(n, vector<int>(sum/2 +1, 0));
-        for(int i = 0; i < n; i++){
-            dp[i][0] = 1;
-        }
-        if(nums[0] <= sum/2) dp[0][nums[0]] = 1; 
+        // vector<vector<int>> dp(n, vector<int>(sum/2 +1, 0));
+        vector<int> prev(sum/2 +1, 0), curr(sum/2 +1, 0);
+
+        prev[0] = curr[0] = 1;
+        if(nums[0] <= sum/2) prev[nums[0]] = 1;
         for(int ind = 1; ind < n; ind++){
             for(int target = 1; target <= sum/2; target++){
-                bool notTake = dp[ind-1][target];
+                bool notTake = prev[target];
                 bool take = false;
                 if(nums[ind] <= target){
-                    take = dp[ind-1][target - nums[ind]];
+                    take = prev[target-nums[ind]];
                 }
-                dp[ind][target] = take||notTake;
-            } 
+                curr[target] = take||notTake;
+            }
+            prev = curr;
         }
-        return dp[n-1][sum/2];
+        return prev[sum/2];
         // return solve(n-1, sum/2, nums, dp);
 
     }
