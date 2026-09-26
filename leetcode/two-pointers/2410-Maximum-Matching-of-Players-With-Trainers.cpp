3c9b59ca88fabdb775c774380babd8faeea48527
Samarth Kaushik <samarthkaushik187@gmail.com>
2026-09-26 15:13:03 +0530

    Add Maximum-Matching-of-Players-With-Trainers (two-pointers)

---
+++ leetcode/two-pointers/2410-Maximum-Matching-of-Players-With-Trainers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        int n = players.size();
+        int m = trainers.size();
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int i = 0, j = 0;
+        int cnt = 0;
+        while(i < n && j < m){
+            if(players[i] <= trainers[j]){
+                i++;
+                j++;
+                cnt++;
+            }else{
+                j++;   
+            }
+        }
+        return cnt;
+    }
+};
